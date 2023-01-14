31f9da0aaeb7db39c66826c69c4aa45aad4c962b
MayRedBeWithYou <mayredbewithyou@gmail.com>
2023-01-14 13:11:04 +0100

    Initial commit
    Added React project w/ Firebase

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hej Chmurko!</h1>
+      <p className="subtitle">
+        Tak, postawiłem już hosting. Love you! ❤
+        </p>
+    </div>
+  )
+}
+
+export default App
